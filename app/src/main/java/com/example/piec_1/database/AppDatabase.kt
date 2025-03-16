dfbdcac5c401c262f2b9b2c6952d43637889ec62
YannLeao <yann.kjleao@ufrpe.br>
2025-03-16 13:00:35 -0300

    fix: Room e armazenamento interno

--- app/src/main/java/com/example/piec_1/database/AppDatabase.kt
+++ app/src/main/java/com/example/piec_1/database/AppDatabase.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.piec_1.model.Medicamento
 import com.example.piec_1.model.Usuario
 
-@Database(entities = [Usuario::class, Medicamento::class], version = 1)
+@Database(
+    entities = [Usuario::class, Medicamento::class],
+    version = 1
+)
+
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
     abstract fun medicamentoDao(): MedicamentoDao
@@ -17,7 +23,7 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
         fun getDatabase(context: Context): AppDatabase {
-            Log.d("Database: ", "Início da criação")
+            Log.d("Database: ", "Início da criação do bando de dados")
             return INSTANCE ?: synchronized(this) {
                 Log.d("Database: ", "Dentro do sychronize")
                 Log.d("Database: ", "$context")
@@ -26,7 +32,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "app_database_db"
                 ).build()
-                Log.d("Database: ", "Finalização do banco de dados")
+                Log.d("Database: ", "Criação do bando de dados finalizado")
                 INSTANCE = instance
                 instance
             }
