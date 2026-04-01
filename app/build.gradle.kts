05fdc84bf81f3b019d7259a58ff3cf2f281055cc
Yann Leão <yann.kjleao@ufrpe.br>
2026-04-01 19:32:49 -0300

    chore: update agp and align Kotlin verrsions (#17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.piec_1"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -49,40 +49,63 @@ android {
 
 dependencies {
 
+    // ANDROID BASICS
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // COMPOSE UI
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // NAVIGATION
     implementation(libs.androidx.navigation.compose)
+
+    // CAMERA / HARDWARE
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.video)
     implementation(libs.androidx.camera.view)
     implementation(libs.androidx.camera.extensions)
-    implementation(libs.androidx.runtime.livedata)
+
+    // MACHINE LEARNING
     implementation(libs.google.play.services.mlkit.text.recognition)
     implementation(libs.google.mlkit.objects.detection)
-    implementation(libs.gson)
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.lifecycle.livedata)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.kotlinx.coroutine.core)
-    implementation(libs.kotlinx.coroutine.android)
+
+    // API
     implementation(libs.squareup.retrofit2.retrofit)
     implementation(libs.squareup.retrofit2.converter.gson)
     implementation(libs.squareup.okhttp3.logging.interceptor)
+    implementation(libs.gson)
+
+    // CONCURRENCY
+    implementation(libs.kotlinx.coroutine.core)
+    implementation(libs.kotlinx.coroutine.android)
+
+    // DATABASE
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // LIFECYCLE / STATE
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.runtime.livedata)
+
+    // BACKGROUND TASKS
     implementation(libs.androidx.work.runtime.ktx)
+
+    // TESTS
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
 }
