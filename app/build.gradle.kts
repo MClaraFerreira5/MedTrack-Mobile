17671a7503a8871849ecd642a39d0a31591ac1d8
Yann Leão <yann.kjleao@ufrpe.br>
2026-04-01 21:27:35 -0300

    chore: update AndroidX dependencies (#17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "com.example.piec_1"
-    compileSdk = 35
+    compileSdk = 36
 
 
     defaultConfig {
         applicationId = "com.example.piec_1"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
