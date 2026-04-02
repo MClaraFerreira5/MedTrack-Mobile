6e76b0b1b427dd287397b31a51662394ebdba660
Yann Leão <yann.kjleao@ufrpe.br>
2026-04-01 21:09:26 -0300

    build: rename project to MedTrack and minor cleanup (#17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,25 +49,27 @@ android {
 
 dependencies {
 
-    // ANDROID BASICS
+    // --- Core ---
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
 
-    // COMPOSE UI
+    // --- Compose ---
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // NAVIGATION
+    // --- Navigation ---
     implementation(libs.androidx.navigation.compose)
 
-    // CAMERA / HARDWARE
+    // --- Camera ---
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
@@ -75,33 +77,33 @@ dependencies {
     implementation(libs.androidx.camera.view)
     implementation(libs.androidx.camera.extensions)
 
-    // MACHINE LEARNING
-    implementation(libs.google.play.services.mlkit.text.recognition)
+    // --- ML Kit ---
+    implementation(libs.google.mlkit.text.recognition)
     implementation(libs.google.mlkit.objects.detection)
 
-    // API
+    // --- Networking ---
     implementation(libs.squareup.retrofit2.retrofit)
     implementation(libs.squareup.retrofit2.converter.gson)
     implementation(libs.squareup.okhttp3.logging.interceptor)
     implementation(libs.gson)
 
-    // CONCURRENCY
-    implementation(libs.kotlinx.coroutine.core)
-    implementation(libs.kotlinx.coroutine.android)
+    // --- Concurrency ---
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 
-    // DATABASE
+    // --- Database ---
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
 
-    // LIFECYCLE / STATE
+    // --- Lifecycle ---
     implementation(libs.androidx.lifecycle.livedata)
     implementation(libs.androidx.runtime.livedata)
 
-    // BACKGROUND TASKS
+    // --- Background ---
     implementation(libs.androidx.work.runtime.ktx)
 
-    // TESTS
+    // --- Testing ---
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
